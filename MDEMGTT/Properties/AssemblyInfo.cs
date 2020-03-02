89b437d5434fb1aac063e3987dd2bd3fe25ab82d
Meysam Karimi <meysam.karimi84@gmail.com>
2020-03-03 00:20:56 +0330

    Add files via upload

---
+++ MDEMGTT/Properties/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+﻿using NMF.Models;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MDEMGTT")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MDEMGTT")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d1c55a4b-5f0b-4269-b116-c7bf533dfd21")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: ModelMetadata("http://grafcet/1.0", "MDEMGTT.Grafcet.nmf")]
+[assembly: ModelMetadata("http://hsm/1.0", "MDEMGTT.HSM.nmf")]
+[assembly: ModelMetadata("http://class/1.0", "MDEMGTT.Class.nmf")]
+[assembly: ModelMetadata("http://momot.big.tuwien.ac.at/architectureCRA/1.0", "MDEMGTT.architectureCRA.nmf")]
+[assembly: ModelMetadata("http://bibtex/1.0", "MDEMGTT.BibTeX.nmf")]
+[assembly: ModelMetadata("http://cpl/1.0", "MDEMGTT.CPL.nmf")]
+[assembly: ModelMetadata("http://petrinet/1.0", "MDEMGTT.PetriNet.nmf")]
